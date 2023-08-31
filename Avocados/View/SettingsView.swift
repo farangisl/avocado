a7d12315600d3ddf8aceb43a84e27d5e16d5124b
Farangis <farangis98@ya.ru>
2023-08-31 15:15:29 +0500

    settings view

--- Avocados/View/SettingsView.swift
+++ Avocados/View/SettingsView.swift
@@ -8,8 +8,80 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State private var enableNotification = true
+    @State private var backgroundRefresh = false
+    
     var body: some View {
-        Text("Settings")
+        VStack(alignment: .center, spacing: 0) {
+            // MARK: - HEADER
+            VStack(alignment: .center, spacing: 5) {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+            }
+            .padding()
+            
+            Form {
+                // MARK: - SECTION #1
+                Section(header: Text("General Settings")) {
+                    Toggle(isOn: $enableNotification) {
+                        Text("Enable notification")
+                    }
+                    
+                    Toggle(isOn: $backgroundRefresh) {
+                        Text("Background refresh")
+                    }
+                }
+                
+                // MARK: - SECTION #2
+                Section(header: Text("Application")) {
+                    if enableNotification {
+                        HStack {
+                            Text("Product").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        HStack {
+                            Text("Compatibility").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                        }
+                        HStack {
+                            Text("Developer").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Farangis")
+                        }
+                        HStack {
+                            Text("Telegram").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("t.me/farangisl")
+                        }
+                        HStack {
+                            Text("Version").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("1.0.0")
+                        }
+                    } else {
+                        HStack {
+                            Text("Personal message").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Happy Coding")
+                        }
+                    }
+                    
+                }
+            }
+        }
+        .frame(maxWidth: 640)
     }
 }
 
